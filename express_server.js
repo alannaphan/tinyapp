0b8fe410a823c8634a1b7bfe4cc32e12d56ec7c2
Alanna <phan.alanna@gmail.com>
2023-10-04 02:29:58 -0400

    added error handling and http concatenation

--- express_server.js
+++ express_server.js
@@ -3,24 +3,23 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const generateRandomString = () => {
-  return(Math.random().toString(36).slice(7));
-}
+  return Math.random().toString(36).slice(7);
+};
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
-});
-
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -32,12 +31,15 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase};
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
-})
+});
 
 app.post("/urls", (req, res) => {
-  const { longURL } = req.body; // 
+  let { longURL } = req.body; 
+  if (!(longURL.includes("http://")) && !(longURL.includes("https://"))) {
+    longURL = "http://" + longURL;
+  }
   const id = generateRandomString();
   urlDatabase[id] = longURL;
   res.redirect(`/urls/${id}`);
@@ -48,11 +50,28 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);  
-});
+  app.get("/urls.json", (req, res) =>{
+    console.log(body);
+  })
+  for (let idCode in urlDatabase) {
+    if (req.params.id === idCode) {
+      const templateVars = {
+        id: req.params.id,
+        longURL: urlDatabase[req.params.id],
+      };
+      res.render("urls_show", templateVars);
+      return;
+    }
+    } 
+      const templateVars = {
+        id: req.params.id,
+        longURL: "Error: invalid Id code, website undefined"
+      }
+    res.render("urls_show", templateVars);
+  }
+);
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
-})
+});
