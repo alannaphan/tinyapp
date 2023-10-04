458ccd3d88ee8179cc45f586725e7e26eb556755
Alanna <phan.alanna@gmail.com>
2023-10-04 16:03:19 -0400

    added update forms for urls_show.ejs

--- express_server.js
+++ express_server.js
@@ -78,3 +78,13 @@ app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[id];
   res.redirect("/urls");
 })
+
+app.post("/urls/:id/update", (req, res) => {
+  const { id } = req.params;
+  let { longURL } = req.body;
+  if (!(longURL.includes("http://")) && !(longURL.includes("https://"))) {
+    longURL = "http://" + longURL;
+  }
+  urlDatabase[id] = longURL;
+  res.redirect(`/urls/${id}`);
+})
